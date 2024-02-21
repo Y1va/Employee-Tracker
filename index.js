8dd491b35386af6ca4d42df7d7dac198ccabeacd
ZeuS <ponzeadrian@gmail.com>
2024-02-21 20:34:20 +1100

    Made the startApp function

--- index.js
+++ index.js
@@ -17,3 +17,62 @@ connection.connect(async (err) => {
 });
 
 // Prompt the user to choose an action
+async function startApp() {
+  try {
+    const answer = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit',
+        ],
+      },
+    ]);
+
+    switch (answer.action) {
+    case 'View all departments':
+        await viewDepartments();
+        break;
+    case 'View all roles':
+        await viewRoles();
+        break;
+    case 'View all employees':
+        await viewEmployees();
+        break;
+    case 'Add a department':
+        await addDepartment();
+        break;
+    case 'Add a role':
+        await addRole();
+        break;
+    case 'Add an employee':
+        await addEmployee();
+        break;
+    case 'Update an employee role':
+        await updateEmployeeRole();
+        break;
+        // Ends the connection
+    case 'Exit':
+        connection.end();
+        break;
+    default:
+      // If answer.action doesn't match any case, it will log the following and calls the startApp function
+        console.log('Invalid action. Please try again.');
+        await startApp();
+        break;
+    }
+  } catch (err) {
+      console.error('An error occured:', err);
+      connection.end();
+      process.exit(1);
+  }
+}
+
